3500b2da814f4a589aae57dee181ef7e9c144c4a
vineetsaurabh <vineetsaurabh@gmail.com>
2018-06-08 00:36:25 +0530

    RG-52 Multipage Report

--- server/src/main/java/com/rolaface/util/PDFGeneratorUtil.java
+++ server/src/main/java/com/rolaface/util/PDFGeneratorUtil.java
@@ -15,26 +15,25 @@ import com.rolaface.entities.FlexError;
 public final class PDFGeneratorUtil {
 
 	private static PDPage PAGE = new PDPage();
-	private static final float PAGE_WIDTH = PAGE.getMediaBox().getWidth(); // 612.0
-	private static final float PAGE_HEIGHT = PAGE.getMediaBox().getHeight(); // 792.0
+	private static final float PAGE_WIDTH = PAGE.getMediaBox().getWidth();
+	private static final float PAGE_HEIGHT = PAGE.getMediaBox().getHeight();
 
 	private static final int PAGE_MARGIN_X = 50;
 	private static final int PAGE_MARGIN_Y = 20;
-	private static final float TABLE_WIDTH = PAGE_WIDTH - 2 * PAGE_MARGIN_X; // 512.0
-	private static final float TABLE_HEIGHT = PAGE_HEIGHT - 2 * PAGE_MARGIN_Y;
+	private static final float TABLE_WIDTH = PAGE_WIDTH - 2 * PAGE_MARGIN_X;
 
 	private static final String TITLE = "List of Errors";
 	private static final PDType1Font TITLE_FONT = PDType1Font.TIMES_BOLD;
 	private static final int TITLE_FONT_SIZE = 20;
 	private static final int TITLE_MARGIN = 20;
 
-	private static final float NEW_PAGE_TABLE_POSITION_Y = PAGE_HEIGHT - PAGE_MARGIN_Y - 2 * TITLE_MARGIN;
+	private static final float NEW_PAGE_TABLE_POSITION_Y = PAGE_HEIGHT - PAGE_MARGIN_Y;
 	private static final float NEW_PAGE_TABLE_TEXT_POSITION_Y = NEW_PAGE_TABLE_POSITION_Y - 15;
 
 	private static final int NO_OF_COLUMNS = 5;
 	private static final int ROW_HEIGHT = 20;
 	private static final int CELL_MARGIN = 5;
-	private static final float[] COL_WIDTH = { 30.0f, 90.0f, TABLE_WIDTH - 160.0f, 20.0f, 20.0f, 0.0f };
+	private static final float[] COL_WIDTH = { 40.0f, 90.0f, TABLE_WIDTH - 170.0f, 20.0f, 20.0f, 0.0f };
 
 	private static final String[] HEADERS = { "", "ERR_CODE", "MESSAGE", "T", "B" };
 	private static final PDType1Font HEADER_FONT = PDType1Font.HELVETICA_BOLD;
@@ -47,6 +46,7 @@ public final class PDFGeneratorUtil {
 	private static float CURRENT_TEXT_POSITION_Y = NEW_PAGE_TABLE_TEXT_POSITION_Y;
 	private static float CURRENT_ROW_POSITION_Y = NEW_PAGE_TABLE_POSITION_Y;
 	private static float CURRENT_ROW_HEIGHT = ROW_HEIGHT;
+	private static float FIRST_ROW_POSITION_Y = 0.0f;
 	private static float END_ROW_POSITION_Y = 0.0f;
 
 	private static int count = 1;
@@ -58,31 +58,50 @@ public final class PDFGeneratorUtil {
 	public static byte[] generateListOfErrors(List<FlexError> flexErrors) {
 		byte[] ret = null;
 		try (PDDocument document = new PDDocument(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-			document.addPage(PAGE);
-			PDPageContentStream contentStream = new PDPageContentStream(document, PAGE);
-			drawTitle(contentStream);
-			drawTable(contentStream, flexErrors);
-			contentStream.close();
+
+			int pageNumber = 1;
+			while (count <= flexErrors.size()) {
+				PDPage page = new PDPage();
+				document.addPage(page);
+				PDPageContentStream contentStream = new PDPageContentStream(document, page);
+				if (pageNumber == 1) {
+					drawTitle(contentStream);
+				}
+				drawTable(contentStream, flexErrors);
+				insertPageNumber(contentStream, pageNumber);
+				contentStream.close();
+				pageNumber++;
+			}
+			count = 1;
+
 			document.save(out);
 			ret = out.toByteArray();
 		} catch (IOException e) {
 			// TODO Exception Handling
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return ret;
 	}
 
-	private static void drawTitle(PDPageContentStream contentStream) throws IOException {
-		float titleWidth = TITLE_FONT.getStringWidth(TITLE) / 1000 * TITLE_FONT_SIZE; // 117.76
-		float titleX = (PAGE_WIDTH - titleWidth) / 2; // 247.12
+	private static void drawTitle(PDPageContentStream contentStream) throws IOException, Exception {
+		float titleWidth = TITLE_FONT.getStringWidth(TITLE) / 1000 * TITLE_FONT_SIZE;
+		float titleX = (PAGE_WIDTH - titleWidth) / 2;
 
 		contentStream.beginText();
 		contentStream.setFont(TITLE_FONT, TITLE_FONT_SIZE);
 		contentStream.newLineAtOffset(titleX, PAGE_HEIGHT - PAGE_MARGIN_Y - TITLE_MARGIN);
 		contentStream.showText(TITLE);
 		contentStream.endText();
+
+		CURRENT_TEXT_POSITION_Y = CURRENT_TEXT_POSITION_Y - 2 * TITLE_MARGIN;
+		CURRENT_ROW_POSITION_Y = CURRENT_ROW_POSITION_Y - 2 * TITLE_MARGIN;
 	}
 
-	private static void drawTable(PDPageContentStream contentStream, List<FlexError> flexErrors) throws IOException {
+	private static void drawTable(PDPageContentStream contentStream, List<FlexError> flexErrors)
+			throws IOException, Exception {
+		FIRST_ROW_POSITION_Y = CURRENT_ROW_POSITION_Y;
+
 		drawRowLine(contentStream);
 		drawHeader(contentStream);
 		drawRows(contentStream, flexErrors);
@@ -93,26 +112,14 @@ public final class PDFGeneratorUtil {
 		CURRENT_ROW_POSITION_Y = NEW_PAGE_TABLE_POSITION_Y;
 	}
 
-	private static void drawRows(PDPageContentStream contentStream, List<FlexError> flexErrors) throws IOException {
-		for (FlexError flexError : flexErrors) {
-			if (CURRENT_ROW_POSITION_Y <= PAGE_MARGIN_Y) {
-				break;
-			}
-			addTextInRow(contentStream, flexError, count++);
-			CURRENT_ROW_HEIGHT = ROW_HEIGHT;
-			drawRowLine(contentStream);
-		}
-		END_ROW_POSITION_Y = CURRENT_ROW_POSITION_Y + ROW_HEIGHT;
-	}
-
-	private static void drawRowLine(PDPageContentStream contentStream) throws IOException {
+	private static void drawRowLine(PDPageContentStream contentStream) throws IOException, Exception {
 		contentStream.moveTo(PAGE_MARGIN_X, CURRENT_ROW_POSITION_Y);
 		contentStream.lineTo(PAGE_MARGIN_X + TABLE_WIDTH, CURRENT_ROW_POSITION_Y);
 		contentStream.stroke();
 		CURRENT_ROW_POSITION_Y -= CURRENT_ROW_HEIGHT;
 	}
 
-	private static void drawHeader(PDPageContentStream contentStream) throws IOException {
+	private static void drawHeader(PDPageContentStream contentStream) throws IOException, Exception {
 		contentStream.setFont(HEADER_FONT, HEADER_FONT_SIZE);
 		for (int j = 0; j < NO_OF_COLUMNS; j++) {
 			contentStream.beginText();
@@ -126,12 +133,22 @@ public final class PDFGeneratorUtil {
 		drawRowLine(contentStream);
 	}
 
+	private static void drawRows(PDPageContentStream contentStream, List<FlexError> flexErrors)
+			throws IOException, Exception {
+		while (!(CURRENT_ROW_POSITION_Y <= PAGE_MARGIN_Y || count > flexErrors.size())) {
+			addTextInRow(contentStream, flexErrors.get(count - 1), count++);
+			CURRENT_ROW_HEIGHT = ROW_HEIGHT;
+			drawRowLine(contentStream);
+		}
+		END_ROW_POSITION_Y = CURRENT_ROW_POSITION_Y + ROW_HEIGHT;
+	}
+
 	private static void addTextInRow(PDPageContentStream contentStream, FlexError flexError, int count)
-			throws IOException {
+			throws IOException, Exception {
 		String[] flexErrorValues = new String[NO_OF_COLUMNS];
 		flexErrorValues[0] = String.valueOf(count);
 		flexErrorValues[1] = flexError.getErrcode();
-		flexErrorValues[2] = flexError.getMessage();
+		flexErrorValues[2] = flexError.getMessage().trim();
 		flexErrorValues[3] = flexError.getErrortype();
 		flexErrorValues[4] = flexError.getBatchtype();
 
@@ -145,9 +162,11 @@ public final class PDFGeneratorUtil {
 		CURRENT_TEXT_POSITION_Y -= CURRENT_ROW_HEIGHT;
 	}
 
-	private static void addTextInCell(PDPageContentStream contentStream, String text, int colCount) throws IOException {
+	private static void addTextInCell(PDPageContentStream contentStream, String text, int colCount)
+			throws IOException, Exception {
 		contentStream.setFont(CELL_FONT, CELL_FONT_SIZE);
-		float textWidth = CELL_FONT.getStringWidth(text) / 1000 * CELL_FONT_SIZE;
+		float textWidth = 0.0f;
+		textWidth = CELL_FONT.getStringWidth(text) / 1000 * CELL_FONT_SIZE;
 		float cellWidth = COL_WIDTH[2];
 		if (colCount == 2 && textWidth > cellWidth) {
 			int length = (int) (cellWidth / (CELL_FONT.getAverageFontWidth() / 1000 * CELL_FONT_SIZE));
@@ -172,14 +191,22 @@ public final class PDFGeneratorUtil {
 		}
 	}
 
-	private static void drawColumns(PDPageContentStream contentStream) throws IOException {
+	private static void drawColumns(PDPageContentStream contentStream) throws IOException, Exception {
 		float nextX = PAGE_MARGIN_X;
 		for (int i = 0; i <= NO_OF_COLUMNS; i++) {
-			contentStream.moveTo(nextX, NEW_PAGE_TABLE_POSITION_Y);
+			contentStream.moveTo(nextX, FIRST_ROW_POSITION_Y);
 			contentStream.lineTo(nextX, END_ROW_POSITION_Y);
 			contentStream.stroke();
 			nextX += COL_WIDTH[i];
 		}
+	}
+
+	private static void insertPageNumber(PDPageContentStream contentStream, int pageNumber)
+			throws IOException, Exception {
+		contentStream.beginText();
+		contentStream.newLineAtOffset(PAGE_WIDTH - PAGE_MARGIN_X / 2, PAGE_MARGIN_Y / 2);
+		contentStream.showText(String.valueOf(pageNumber));
+		contentStream.endText();
 	}
 
 	public static String[] wrapText(String text, int len) {
