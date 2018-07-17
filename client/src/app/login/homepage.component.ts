b019b7af2194047cf0d04d7f3e6a23a67a254d9a
Vineet Saurabh <vineetsaurabh@gmail.com>
2018-07-17 18:06:25 +0530

    RG-55 Homepage and GlobalToolbar segregation (#110)

--- client/src/app/login/homepage.component.ts
+++ client/src/app/login/homepage.component.ts
@@ -1,36 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { TokenStorage } from './token.storage';
-import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
+
+export interface ErrorCategory {
+    name: string;
+    color: string;
+}
 
 @Component({
-  selector: 'global-toolbar',
-  templateUrl: './homepage.component.html'
+    selector: 'dashboard',
+    templateUrl: './homepage.component.html'
 })
-export class HomepageComponent implements OnInit {
-
-    title = 'Knowledge Base';
-    tabs = ['List Users', 'Find User', 'List Errors', 'Find Error'];
-    currentUser : string = this.token.getCurrentUser();
-
-    constructor(
-      private router: Router,
-      private route: ActivatedRoute,
-      private token: TokenStorage,
-      private authService: AuthService) {
-    }
-    
-    //TODO: Use guard and remove this method
-    ngOnInit(): void {
-      if(!this.authService.isAuthenticated()) {
-        this.router.navigate(['login']);
-      }
-    }
-    
-
-    logout(): void {
-      this.token.signOut();
-      this.router.navigate(['login']);
-    }
-
+export class HomepageComponent {
+    errorCategories: ErrorCategory[] = [
+        {name: 'Flexcube', color: 'lightsteelblue'},
+        {name: 'Database', color: 'lavender'},
+        {name: 'Acumen', color: 'lightsteelblue'},
+        {name: 'Mobile App', color: 'lavender'},
+        {name: 'ERP', color: 'lightsteelblue'},
+        {name: 'FCDB', color: 'lavender'},
+    ];
 }
