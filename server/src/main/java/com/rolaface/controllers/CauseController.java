493beeb7d52c728fb4988b9f4680bebfd98a3db9
Vineet Saurabh <vineetsaurabh@gmail.com>
2018-08-05 23:42:29 -0700

    RG-64 Global cors for AWS hosting (#129)

--- server/src/main/java/com/rolaface/controllers/CauseController.java
+++ server/src/main/java/com/rolaface/controllers/CauseController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import com.rolaface.services.CauseService;
 import com.rolaface.services.FlexErrorCauseService;
 import com.rolaface.services.UserService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping({ "/cause" })
 public class CauseController {
