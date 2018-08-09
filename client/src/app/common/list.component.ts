a479a587c25a98d17b98cd0fc05024ce914f551f
vineetsaurabh <vineetsaurabh@gmail.com>
2018-08-09 22:09:47 +0530

    RG-50 Master Checkbox

---
+++ client/src/app/common/list.component.ts
@@ -0,0 +1,32 @@
+import { MatDialogRef, MatDialog } from "@angular/material";
+import { TableConfiguratorComponent } from "../util/table-configurator.component";
+import { Component } from "@angular/core";
+
+@Component({})
+export class ListComponent {
+
+    allColumns = [];
+    displayedColumns = this.allColumns;
+
+    constructor(
+        protected dialog: MatDialog) {
+    }
+
+    openTableConfigurator() {
+        let dialogRef: MatDialogRef<TableConfiguratorComponent>;
+        dialogRef = this.dialog.open(TableConfiguratorComponent, {
+            data: { 'allColumns': this.allColumns, 'displayedColumns': this.displayedColumns },
+            width: '200px',
+        });
+        return dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.displayedColumns = result;
+            }
+        });
+    }
+
+    printErrors() {
+        window.print();
+    }
+
+}
